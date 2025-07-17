e3b4a187ca7f3e7717595b3e6706c6e197f0ca12
jxskiss <jxskiss@126.com>
2025-07-18 00:41:50 +0800

    Remove cloning ohmyzsh plugins script

--- run_once_install_ohmyzsh_plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ -d "$ZSH_CUSTOM/plugins" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
-fi
